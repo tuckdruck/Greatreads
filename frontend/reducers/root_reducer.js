9415ef0b1413137a586fbc384b63752be5e59eb8
avitaldrucker <avitaldrucker@gmail.com>
2017-02-15 19:34:12 -0500

    Set up front-end functionality for books and bookshelves: refine books and bookshelves controllers, add API Util methods and action creators for books and bookshelves, make new reducers, and start work on MyBooksPage components

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,6 +1,10 @@
 import SessionReducer from './session_reducer';
+import BookshelvesReducer from './bookshelves_reducer';
+import BooksReducer from './books_reducer';
 import { combineReducers } from 'redux';
 
 export default combineReducers({
-  session: SessionReducer
+  session: SessionReducer,
+  bookshelves: BookshelvesReducer,
+  books: BooksReducer
 });
