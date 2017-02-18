c9c934e6662682fcfd89635948a96366e30ed952
avitaldrucker <avitaldrucker@gmail.com>
2017-02-18 17:35:17 -0500

    Fix bug! with deleting book

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,10 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
 import RootReducer from '../reducers/root_reducer';
 import thunk from 'redux-thunk';
+import createLogger from 'redux-logger';
+
+const logger = createLogger();
 
 
 const configureStore = (preloadedState = {}) => {
-  return createStore(RootReducer, preloadedState, applyMiddleware(thunk));
+  return createStore(RootReducer, preloadedState, applyMiddleware(thunk, logger));
 };
 
 export default configureStore;
