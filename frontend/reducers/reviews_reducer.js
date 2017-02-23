d0ee60f4b39a0b89a28026655bc9b026feb9f536
avitaldrucker <avitaldrucker@gmail.com>
2017-02-22 21:56:38 -0500

    Debug problem with loading reviews

--- frontend/reducers/reviews_reducer.js
+++ frontend/reducers/reviews_reducer.js
@@ -1,6 +1,7 @@
 import { RECEIVE_REVIEWS } from '../actions/review_actions';
 
 const ReviewsReducer = (state = {}, action) => {
+  debugger
   switch(action.type) {
     case RECEIVE_REVIEWS:
       return action.reviews;
