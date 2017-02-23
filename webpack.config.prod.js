bc4f694284ddc36e293ed8c71baedc2550fc8416
avitaldrucker <avitaldrucker@gmail.com>
2017-02-23 13:24:21 -0500

    Reconfigure to remove heroku minification warning

---
+++ webpack.config.prod.js
@@ -0,0 +1,36 @@
+var webpack = require("webpack");
+
+module.exports = {
+  entry: "./frontend/greatreads",
+  output: {
+    filename: "./app/assets/javascripts/bundle.js"
+  },
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: [".js", ".jsx" ]
+  }
+};
