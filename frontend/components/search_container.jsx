b1083266de10d737540a14567b745e992682f31f
Avital Drucker <avitaldrucker@gmail.com>
2017-02-23 15:49:07 -0500

    Complete search bar component functionality, so that it redirects to the corresponding book show page

--- frontend/components/search_container.jsx
+++ frontend/components/search_container.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import booksArray from '../selectors/books_selector';
+import { fetchBooksForSearch } from '../actions/book_actions';
+import { hashHistory } from 'react-router';
 
 class Search extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = { inputVal: "" };
-    this.selectBook = this.selectName.bind(this);
+    this.selectBook = this.selectBook.bind(this);
     this.handleInput = this.handleInput.bind(this);
   }
 
@@ -21,35 +24,36 @@ class Search extends React.Component {
   matches() {
     const matches = [];
 
-    this.props.books.forEach((book, index) => {
-      let sub = book.slice(0, this.state.inputVal.length);
+    if (this.state.inputVal.length > 0) {
+      this.props.books.forEach((book, index) => {
+        let sub = book.title.slice(0, this.state.inputVal.length);
 
-      if (sub.toLowerCase() === this.state.inputVal.toLowerCase()) {
-        matches.push(book);
-      }
-    });
+        if (sub.toLowerCase() === this.state.inputVal.toLowerCase()) {
+          matches.push(book);
+        }
+      });
 
-    if (matches.length === 0 && this.state.inputVal.length > 0) {
-      matches.push('No matches');
     }
 
     return matches;
   }
 
-  selectBook() {
-
+  selectBook(bookId) {
+    return (e) => {
+      hashHistory.push(`books/${bookId}`);
+    };
   }
 
   render() {
-    let results = this.matches().map((result, index) => {
-      return (<li key={index} onClick={this.selectBook}>{result}</li>);
+    let bookResults = this.matches().map((book, index) => {
+      return (<li key={index} onClick={this.selectBook(book.id)}>{book.title}</li>);
     });
 
     return(
-      <div>
+      <div className="search">
         <input onChange={this.handleInput} value={this.state.inputVal} placeholder="Search" />
-        <ul>
-          {results}
+        <ul className="search-results">
+          {bookResults}
         </ul>
       </div>
     );
@@ -58,9 +62,15 @@ class Search extends React.Component {
 
 const mapStateToProps = state => {
   return {
-    books: this.props.booksForSearch,
-    booksLoading: this.props.loading.booksLoading
+    books: booksArray(state.booksForSearch),
+    booksLoading: state.loading.booksLoading
   };
 };
 
-export default connect(mapStateToProps, null)(Search);
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchBooksForSearch: () => { return dispatch(fetchBooksForSearch()); }
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Search);
