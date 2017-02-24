fd0aedcb71c821e27b8be326ce30b1aa8403f2a8
Avital Drucker <avitaldrucker@gmail.com>
2017-02-23 20:10:46 -0500

    Create and style search results page

---
+++ frontend/components/search_results_page.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import booksArray from '../selectors/books_selector';
+import HeaderContainer from './header_container';
+import Footer from './footer';
+import { hashHistory } from 'react-router';
+
+
+class SearchResultsPage extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.selectBook = this.selectBook.bind(this);
+  }
+
+  selectBook(bookId) {
+    return (e) => {
+      hashHistory.push(`books/${bookId}`);
+    };
+  }
+
+  render() {
+    const searchResultLis = this.props.books.map((book, index) => {
+      return (
+        <li className="results searchpage" key={index}>
+          <img src={book.cover_image_url} className="search-thumb" onClick={this.selectBook(book.id).bind(this)}/>
+          <div>
+            <h3 onClick={this.selectBook(book.id).bind(this)}>{book.title}</h3>
+            <span>by {book.author}</span>
+            <span>{book.average_rating} avg rating</span>
+          </div>
+        </li>);
+    });
+
+    return(
+      <main className="overall-search">
+        <HeaderContainer />
+        <main className="body searchpage">
+          <h1>Search Results</h1>
+          <ul>
+            {searchResultLis}
+          </ul>
+        </main>
+        <Footer />
+      </main>
+    );
+  }
+
+
+}
+
+
+const mapStateToProps = state => {
+  return {
+    books: booksArray(state.booksForSearch)
+  };
+};
+
+export default connect(mapStateToProps, null)(SearchResultsPage);
