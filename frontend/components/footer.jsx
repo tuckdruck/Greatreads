2644ebb011890e473fe94d97efd78dfbe3345105
Avital Drucker <avitaldrucker@gmail.com>
2017-02-26 15:07:20 -0500

    Add contact information to footer

--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 const Footer = (props) => {
   return (
     <footer className="footer">
-      <span>© 2017 Greatreads</span>
+      <ul>
+        <li className="small-footer">© 2017 Greatreads</li>
+        <li className="small-footer">Avital Drucker</li>
+        <li className="small-footer">avitaldrucker@gmail.com</li>
+        <li><a href="https://www.linkedin.com/in/avitaldrucker"><i className="fa fa-linkedin-square" aria-hidden="true"></i></a></li>
+        <li><a href="https://www.github.com/avitaldrucker"><i className="fa fa-github" aria-hidden="true"></i></a></li>
+      </ul>
     </footer>
   );
 };
