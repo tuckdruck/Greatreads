eee355d8f46d5c8132be2d396f6445a1bf8f8ee1
avitaldrucker <avitaldrucker@gmail.com>
2017-03-16 14:04:43 -0400

    Modify about text and add portfolio website link

--- frontend/components/about.jsx
+++ frontend/components/about.jsx
@@ -26,11 +26,12 @@ export default class About extends React.Component {
     return(
       <div className="about">
         <button onClick={this.props.toggleAbout} className="close-about">X</button>
-        <p>Greatreads is a clone of Goodreads cloned by Avital Drucker, graduate of App Academy in New York.</p>
+        <p>Greatreads is a book app inspired by Goodreads and created by Avital Drucker, a web developer based in New York.</p>
         <p className="email">avitaldrucker@gmail.com</p>
         <figure className="icon-container">
-          <a href="https://www.linkedin.com/in/avitaldrucker"><i className="fa fa-linkedin-square" aria-hidden="true"></i></a>
-          <a href="https://www.github.com/avitaldrucker"><i className="fa fa-github" aria-hidden="true"></i></a>
+          <a href="http://www.avitaldrucker.com"><i className="fa fa-globe" aria-hidden="true"></i></a>
+          <a href="http://www.github.com/avitaldrucker"><i className="fa fa-github" aria-hidden="true"></i></a>
+          <a href="http://www.linkedin.com/in/avitaldrucker"><i className="fa fa-linkedin-square" aria-hidden="true"></i></a>
         </figure>
       </div>
     );
